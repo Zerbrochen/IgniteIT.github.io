206b294b3f0cabcad294e020d25b455a0d0fa810
Triston Howard <31514899+Zerbrochen@users.noreply.github.com>
2017-09-19 09:03:19 -0500

    Update local.js

--- js/local.js
+++ js/local.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
    $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+
+
+<script src="https://www.gstatic.com/firebasejs/4.3.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD0Vx2Bfil4ZJqQR0W3Gpb7aJZyeVAynIU",
+    authDomain: "ignitedb-253e0.firebaseapp.com",
+    databaseURL: "https://ignitedb-253e0.firebaseio.com",
+    projectId: "ignitedb-253e0",
+    storageBucket: "ignitedb-253e0.appspot.com",
+    messagingSenderId: "673494478669"
+  };
+  firebase.initializeApp(config);
+</script>
         
